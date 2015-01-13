27b7731918e7a65ed6b6f80c1a95ee16f1c4e91a
riken312 <riken312@gmail.com>
2015-01-13 13:51:51 +0530

    Added processing sketch
    Added simple mouse interaction with p5.js

---
+++ js/sketch.js
@@ -0,0 +1,17 @@
+function setup() 
+{
+  
+  createCanvas(500,500);
+  smooth();
+  noStroke();
+  fill(50,50);
+
+
+}
+
+function draw() 
+{
+  // put drawing code here
+ellipse(mouseX,mouseY,50,50);
+
+}
